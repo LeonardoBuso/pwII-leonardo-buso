6daa63780bb8a9bdbd1e9ad8f9a9839331300921
Leonardo Buso <leobuso.souza@gmail.com>
2025-05-14 10:34:04 -0300

    Exercícios 01

---
+++ Exercícios01/exercício03.php
@@ -0,0 +1,18 @@
+<html>
+    <body>
+        <form method = "post">
+            Base maior
+            <input type="text" name = "B"> <br>
+            Base menor
+            <input type="text" name = "b"> <br>
+            Altura
+            <input type="text" name = "h"> <br>
+            <input type="submit">
+            <input type="reset">
+        </form>
+
+        <?php
+            $baseMaior = $_POST ["B"]
+        ?>
+    </body>
+</html>
